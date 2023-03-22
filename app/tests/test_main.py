f685987a7f8dfbdbd617020e0fda4e4295390c65
ttyysea <111670925+ttyysea@users.noreply.github.com>
2023-03-22 14:25:45 +0700

    Update test_main.py

--- app/tests/test_main.py
+++ app/tests/test_main.py
@@ -8,7 +8,7 @@ client = TestClient(app)
 def test_read_main():
     response = client.get("/")
     assert response.status_code == 200
-    assert response.json() == {"Hello": "World"}
+    assert response.json() == {"hello": "World"}
 
 def test_GET_hello_Phet():
     name = "Phet"
@@ -19,9 +19,6 @@ def test_GET_hello_Phet():
     assert actual_result.json() == expected_result
 
 def test_POST_hello_Jack():
-    name = "Jack"
-    url = f"/callname/{name}"
-    expected_result = {"hello": name}
-    actual_result = client.post(url)
-    assert actual_result.status_code == 200
-    assert actual_result.json() == expected_result
+    response = client.post("/callname")
+    assert response.status_code == 200
+    assert response.json() == {"hello": "Jack"}
