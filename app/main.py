d14307bf16924848784dbfc756201ed04ea9bdda
ttyysea <111670925+ttyysea@users.noreply.github.com>
2023-03-22 14:25:10 +0700

    Update main.py

--- app/main.py
+++ app/main.py
@@ -5,15 +5,15 @@ app = FastAPI()
 
 @app.get("/")
 def read_root():
-    return {"Hello": "World"}
+    return {"hello": "World"}
 
 
 @app.get("/callname/{name}")
 def read_name(name: str = None):
     return {"hello": name}
 
-@app.post("/callname/{name}")
-def read_name(name: str = None):
+@app.post("/callname")
+def post_call_name(name = "Jack"):
     return {"hello": name}
 
 
