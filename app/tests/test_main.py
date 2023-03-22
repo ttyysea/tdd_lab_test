e117c7615fae461380b9edf120d0c56c7caf896c
ttyysea <111670925+ttyysea@users.noreply.github.com>
2023-03-22 13:36:53 +0700

    Create test_main.py

---
+++ app/tests/test_main.py
@@ -0,0 +1,27 @@
+from fastapi.testclient import TestClient
+
+from app.main import app
+
+client = TestClient(app)
+
+
+def test_read_main():
+    response = client.get("/")
+    assert response.status_code == 200
+    assert response.json() == {"Hello": "World"}
+
+def test_GET_hello_Phet():
+    name = "Phet"
+    url = f"/callname/{name}"
+    expected_result = {"hello": name}
+    actual_result = client.get(url)
+    assert actual_result.status_code == 200
+    assert actual_result.json() == expected_result
+
+def test_POST_hello_Jack():
+    name = "Jack"
+    url = f"/callname/{name}"
+    expected_result = {"hello": name}
+    actual_result = client.post(url)
+    assert actual_result.status_code == 200
+    assert actual_result.json() == expected_result
